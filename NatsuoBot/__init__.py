fc98fd878c8b12be0e3644b47138264f17d79c35
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-12 09:20:26 +0530

    Update __init__.py

--- NatsuoBot/__init__.py
+++ NatsuoBot/__init__.py
@@ -2,25 +2,32 @@ import logging
 import os
 import sys
 import time
+import spamwatch
 from redis import StrictRedis
 from pyrogram import Client, errors
 
+from aiohttp import ClientSession
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 import telegram.ext as tg
 from telethon import TelegramClient
 
-StartTime = time.time()
+since_time_start = time.time()
 
 # enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('log.txt'),
+              logging.StreamHandler()],
     level=logging.INFO)
 
 LOGGER = logging.getLogger(__name__)
 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
-    quit(1)
+    LOGGER.error(
+        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    )
+    sys.exit(1)
 
 ENV = bool(os.environ.get('ENV', False))
 
@@ -31,159 +38,96 @@ if ENV:
         OWNER_ID = int(os.environ.get('OWNER_ID', None))
     except ValueError:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
-
-    MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
+    
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
 
     try:
-        SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
+        SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "").split()}
+        DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
     except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
+        raise Exception(
+            "Your sudo or dev users list does not contain valid integers.")
 
     try:
-        SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
+        SUPPORT_USERS = {int(x) for x in os.environ.get("SUPPORT_USERS", "").split()}
     except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
+        raise Exception(
+            "Your support users list does not contain valid integers.")
 
     try:
-        SPAMMERS = set(int(x) for x in os.environ.get("SPAMMERS", "").split())
+        WHITELIST_USERS = {int(x) for x in os.environ.get("WHITELIST_USERS", "").split()}
     except ValueError:
-        raise Exception("Your spammers users list does not contain valid integers.")
+        raise Exception(
+            "Your whitelisted users list does not contain valid integers.")
+    
+    START_IMG = os.environ.get('START_IMG', "")
+    EVENT_LOGS = os.environ.get('EVENT_LOGS', None)
+    MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None) 
+    ERROR_DUMP = os.environ.get('ERROR_DUMP', None)
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    JOIN_LOGGER = os.environ.get('JOIN_LOGGER', None)
 
-    try:
-        WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    try:
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())    
-    except ValueError:  
-        raise Exception("Your developer users list does not contain valid integers.")
-
-    GBAN_LOGS = os.environ.get('GBAN_LOGS', None)
-    REDIS_URL =os.environ.get('REDIS_URL')
     WEBHOOK = bool(os.environ.get('WEBHOOK', False))
     URL = os.environ.get('URL', "")  # Does not contain token
     PORT = int(os.environ.get('PORT', 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
-    API_ID = os.environ.get("API_ID", None)
-    API_HASH = os.environ.get("API_HASH", None)
-    DB_URI = os.environ.get('DATABASE_URL')
+
+    API_ID = os.environ.get('API_ID', None)
+    API_HASH = os.environ.get('API_HASH', None)
+    DB_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     DONATION_LINK = os.environ.get('DONATION_LINK')
     LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
+    NO_LOAD = os.environ.get("NO_LOAD", "").split()
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
+    STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
     STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
-    WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
-    LASTFM_API_KEY = os.environ.get('LASTFM_API_KEY', "")   
-    ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
+    WORKERS = int(os.environ.get('WORKERS', 8)) 
+
     CASH_API_KEY = os.environ.get('CASH_API_KEY', None)
+    YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY', None)
     TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
-    API_WEATHER  = os.environ.get('API_OPENWEATHER',False)
     AI_API_KEY = os.environ.get('AI_API_KEY', None)
     WALL_API = os.environ.get('WALL_API', None)
-    CUSTOM_CMD = os.environ.get('CUSTOM_CMD', ('/', '!'))
-    STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
-    SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
+    API_WEATHER = os.environ.get('API_WEATHER', None)
 
-    # TMP_DOWNLOAD_DIRECTORY= os.environ.get('TMP_DOWNLOAD_DIRECTORY', /)
-    MAL_CLIENT_ID = os.environ.get('MAL_CLIENT_ID', "")    
-    MAL_ACCESS_TOKEN = os.environ.get('MAL_ACCESS_TOKEN', "")
-    MAL_REFRESH_TOKEN = os.environ.get('MAL_REFRESH_TOKEN', "")
-    
+    SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
+    SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
+    SPAMWATCH = os.environ.get('SPAMWATCH_API', None)
+    REDIS_URL = os.environ.get('REDIS_URL', None)
+    CUSTOM_CMD = os.environ.get('CUSTOM_CMD', ('/', '!'))
+    REPOSITORY = os.environ.get('REPOSITORY', "")
+    IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
+    IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+
+
     try:
         WHITELIST_CHATS = {int(x) for x in os.environ.get('WHITELIST_CHATS', "").split()}
     except ValueError:
         raise Exception(
             "Your blacklisted chats list does not contain valid integers.")
 
+    try:
+        BLACKLIST_CHATS = {int(x) for x in os.environ.get('BLACKLIST_CHATS', "").split()}
+    except ValueError:
+        raise Exception(
+            "Your blacklisted chats list does not contain valid integers.")
 
-    try: 
-         BL_CHATS = set(int(x) for x in os.environ.get('BL_CHATS', "").split())  
-    except ValueError:     
-          raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from NatsuoBot.config import Development as Config
-    TOKEN = Config.API_KEY
-
-    try:
-        OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
-
-    MESSAGE_DUMP = Config.MESSAGE_DUMP
-    OWNER_USERNAME = Config.OWNER_USERNAME
-
-    try:
-        SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or [])
-        DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
-
-    try:
-        SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or [])
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        SPAMMERS = set(int(x) for x in Config.SPAMMERS or [])
-    except ValueError:
-        raise Exception("Your spammers users list does not contain valid integers.")
-
-    try:
-        WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or [])
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    try:
-        DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])   
-    except ValueError:  
-        raise Exception("Your developer users list does not contain valid integers.")
-
-    GBAN_LOGS = Config.GBAN_LOGS
-    WEBHOOK = Config.WEBHOOK
-    URL = Config.URL
-    PORT = Config.PORT
-    CERT_PATH = Config.CERT_PATH
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
-    DB_URI = Config.SQLALCHEMY_DATABASE_URI
-    DONATION_LINK = Config.DONATION_LINK
-    LOAD = Config.LOAD
-    NO_LOAD = Config.NO_LOAD
-    DEL_CMDS = Config.DEL_CMDS
-    STRICT_GBAN = Config.STRICT_GBAN
-    WORKERS = Config.WORKERS
-    BAN_STICKER = Config.BAN_STICKER
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    TIME_API_KEY = Config.TIME_API_KEY
-    API_OPENWEATHER = Config.API_OPENWEATHER
-    AI_API_KEY = Config.AI_API_KEY
-    WALL_API = Config.WALL_API
-    REDIS_URL = Config.REDIS_URL
-    STRICT_GMUTE = Config.STRICT_GMUTE
-    # TMP_DOWNLOAD_DIRECTORY=Config.TMP_DOWNLOAD_DIRECTORY=
-    MOE_API = Config.MOE_API   
-    MAL_ACCESS_TOKEN = Config.MAL_ACCESS_TOKEN
-    MAL_REFRESH_TOKEN = Config.MAL_REFRESH_TOKEN        
-
-
-   
-    try:
-        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
-    except ValueError:
-        raise Exception ("Your blacklisted chats list does not contain valid integers.")
-
+     LOGGER.warning("Unknown Crash!")
+    
+    
 
 SUDO_USERS.add(OWNER_ID)
-SUDO_USERS.add(712008424)
-
-
 DEV_USERS.add(OWNER_ID)
+
+if SPAMWATCH == None:
+    spamwtc = None
+    LOGGER.warning("Invalid spamwatch api")
+else:
+    spamwtc = spamwatch.Client(SPAMWATCH)
+
 
 REDIS = StrictRedis.from_url(REDIS_URL,decode_responses=True)
 try:
@@ -192,20 +136,53 @@ try:
 except BaseException:
     raise Exception("Your redis server is not alive, please check again.")
 
+
+api_id = API_ID
+api_hash = API_HASH
+
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-oko = TelegramClient("kora", API_ID, API_HASH)
-kp = Client("koraPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+client = TelegramClient("saber", API_ID, API_HASH)
+pbot = Client("saberPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+mongo_client = MongoClient(MONGO_DB_URI)
+db = mongo_client.wbb
 dispatcher = updater.dispatcher
+
+kp = Client(":memory:", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN, workers=min(32, os.cpu_count() + 4))
+
+
+async def get_entity(client, entity):
+    entity_client = client
+    if not isinstance(entity, Chat):
+        try:
+            entity = int(entity)
+        except ValueError:
+            pass
+        except TypeError:
+            entity = entity.id
+        try:
+            entity = await client.get_chat(entity)
+        except (PeerIdInvalid, ChannelInvalid):
+            for pbot in apps:
+                if pbot != client:
+                    try:
+                        entity = await pbot.get_chat(entity)
+                    except (PeerIdInvalid, ChannelInvalid):
+                        pass
+                    else:
+                        entity_client = pbot
+                        break
+            else:
+                entity = await pbot.get_chat(entity)
+                entity_client = pbot
+    return entity, entity_client
 
 SUDO_USERS = list(SUDO_USERS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
 WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
-# TIGER_USERS = list(TIGER_USERS)
-# SPAMMERS = list(SPAMMERS)
 
 # Load at end to ensure all prev variables have been set
-from NatsuoBot.modules.helper_funcs.handlers import CustomCommandHandler
+from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler
 
 if CUSTOM_CMD and len(CUSTOM_CMD) >= 1:
     tg.CommandHandler = CustomCommandHandler
